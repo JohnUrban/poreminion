bf49694f2a3f8c1e25bc28b00663c86037ef082c
John Urban <mr.john.urban@gmail.com>
2015-02-24 17:58:26 -0500

    Bumped the version from 0.1.0 to 0.2.0

--- poreminion/scripts/poreminion-script.py
+++ poreminion/scripts/poreminion-script.py
@@ -1,5 +1,5 @@
 #!C:\Anaconda\python.exe
-# EASY-INSTALL-SCRIPT: 'poreminion==0.1.0','poreminion'
-__requires__ = 'poreminion==0.1.0'
+# EASY-INSTALL-SCRIPT: 'poreminion==0.2.0','poreminion'
+__requires__ = 'poreminion==0.2.0'
 import pkg_resources
-pkg_resources.run_script('poreminion==0.1.0', 'poreminion')
+pkg_resources.run_script('poreminion==0.2.0', 'poreminion')

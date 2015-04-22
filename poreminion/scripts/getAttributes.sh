b22427ca4c4820b1deb4633290d068a03457111b
John Urban <mr.john.urban@gmail.com>
2015-04-22 15:33:37 -0400

    added getAttributes.sh

---
+++ poreminion/scripts/getAttributes.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+ echo "Usage: getAttributes.sh file.fast5"
+ exit 0
+fi
+
+SEED=$RANDOM
+
+h5dump -n 1 $1 | grep "attribute" | cut -f 4 -d " " > $SEED.poreminion.getattributes.tmp.txt
+
+while read line; do
+ value=`h5dump -a $line $1 | grep "(0)" | awk '{gsub(/\"/,""); print $2}'`
+ echo -e $line "\t" $value
+done < $SEED.tmp.txt
+
+rm ${SEED}.poreminion.getattributes.tmp.txt

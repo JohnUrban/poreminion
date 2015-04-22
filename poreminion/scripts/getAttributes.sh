916057cd0f37c4c03c00d5901ca7cda565018181
John Urban <mr.john.urban@gmail.com>
2015-04-22 15:40:21 -0400

    updated getAttributes.sh

--- poreminion/scripts/getAttributes.sh
+++ poreminion/scripts/getAttributes.sh
@@ -12,6 +12,6 @@ h5dump -n 1 $1 | grep "attribute" | cut -f 4 -d " " > $SEED.poreminion.getattrib
 while read line; do
  value=`h5dump -a $line $1 | grep "(0)" | awk '{gsub(/\"/,""); print $2}'`
  echo -e $line "\t" $value
-done < $SEED.tmp.txt
+done < $SEED.poreminion.getattributes.tmp.txt
 
 rm ${SEED}.poreminion.getattributes.tmp.txt
